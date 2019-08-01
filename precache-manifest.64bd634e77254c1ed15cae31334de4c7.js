068ea750890d37e5bc4ce0b50b9eda8457833984
sagun <sshrestha.me@gmail.com>
2019-08-01 19:18:20 -0400

    Updates

---
+++ precache-manifest.64bd634e77254c1ed15cae31334de4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab1d469299ab2ee296cbc7fdc5f2ac8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5dcd62c32894226f03a2",
+    "url": "/monsters-rolodex/static/css/main.e322f10d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5dcd62c32894226f03a2",
+    "url": "/monsters-rolodex/static/js/main.723d29e0.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
